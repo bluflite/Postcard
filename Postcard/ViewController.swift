d83e6d34f1778fdc81117fdad0837e50679d24f7
Josh Bontrager <joshbontrager@Joshs-MacBook-Pro.local>
2015-05-01 21:49:51 -0400

    Third rev moved labels

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -29,7 +29,6 @@ class ViewController: UIViewController {
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
-        // Adding a Comment here to test commit DERP
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.grayColor()
